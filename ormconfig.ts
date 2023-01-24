2b226575e7034f3f818631e757666c2340f348bc
Leandro Joan Bustos <leandro@medhubtech.com>
2023-01-24 11:06:09 -0300

    feature/Add orm and ts config for proyect and database definitions

---
+++ ormconfig.ts
@@ -0,0 +1,28 @@
+import { DataSourceOptions } from 'typeorm/data-source'
+import {config} from './src/core/config'
+
+const ormconfig: DataSourceOptions = {
+    "type": "postgres",
+    "host": config.postgres.host,
+    "port": config.postgres.port,
+    "username": config.postgres.username,
+    "password": config.postgres.password,
+    "database": config.postgres.database,
+    "synchronize": false,
+    "logging": true,
+    "entities": [__dirname + "/src/entities/*.{js,ts}"],
+    "migrations": ["src/migration/*.ts"],
+    "subscribers": ["src/subscriber/*.ts"],
+    "extra": {
+        "pool":{
+            "max": 100,
+            "min": 0,
+            "evictionRunIntervalMillis": 10000,
+            "testOnBorrow": true,
+            "testOnReturn": true,
+            "testWhileIdle": true
+        }
+    }
+}
+
+export default ormconfig
