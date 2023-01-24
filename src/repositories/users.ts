e8ec325931cda36dd546369cf4a2550fc696707a
Leandro Joan Bustos <leandro@medhubtech.com>
2023-01-24 11:04:29 -0300

    feature/Add repositories folder structure

---
+++ src/repositories/users.ts
@@ -0,0 +1,31 @@
+import { UpdateResult } from 'typeorm';
+import { UserState } from './../constants/user';
+import * as userEntities from '../entities/user';
+import { User } from '../interfaces/user'
+import { postgresqlDb } from './../core/database/postgresql';
+
+const userRepository = postgresqlDb.getRepository(userEntities.User)
+
+//TODO
+//REEMPLAZAR number POR string YA QUE SERA UN UUID
+export const getUserById = async (userId: number) => 
+    userRepository.find({
+        where: {
+            id: userId
+        }
+    })
+
+export const getUserByPaswordProvisional = async (passwordProvisional: string):Promise<User[]> =>
+    userRepository.find({
+        where: {
+            password: passwordProvisional
+        }
+    })
+
+    //TODO MODULARIZAR PARA REUTILIZAR FUNCION
+export const updateUserProvisional = async (userId: number, password: string, state: UserState):Promise<UpdateResult> => 
+    userRepository.update({ id: userId }, { password, state, modified_at: new Date() })
+
+export const updateUserState = async (userId: number, state: UserState):Promise<UpdateResult> => 
+    userRepository.update({ id: userId }, { state })
+
