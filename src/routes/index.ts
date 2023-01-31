9ac110d803161430d818f4acea06018083ead3a7
Leandro Joan Bustos <leandro@medhubtech.com>
2023-01-31 10:57:07 -0300

    chore/Add use routes for login routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
-import signupRoutes from './signup/index'
+import signupRoutes from './signup'
+import loginRoutes from './login'
 
 const router = Router()
 
 router.use('/signup', signupRoutes)
+router.use('/login', loginRoutes)
 
 export default router
