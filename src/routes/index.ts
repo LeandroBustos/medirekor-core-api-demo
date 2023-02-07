a7dc25f5b637619da198e1ee2d9a4f95d2e09434
Leandro Joan Bustos <leandro@medhubtech.com>
2023-02-07 10:46:13 -0300

    feature/Add middlewares and admin routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,17 @@
+import { checkUserAccess, getUserDataFromAccessToken } from './../middlewares/user';
 import { Router } from 'express';
 
 import signupRoutes from './signup'
 import loginRoutes from './login'
+import adminRoutes from './admin'
+import { UsertTypes } from '../constants/user';
 
 const router = Router()
 
 router.use('/signup', signupRoutes)
 router.use('/login', loginRoutes)
+
+router.use(getUserDataFromAccessToken)
+router.use(checkUserAccess(UsertTypes.SYSADMIN), adminRoutes)
 
 export default router
