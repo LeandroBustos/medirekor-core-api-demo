301878a7a41cff389e55009f6ffef92354bbecb5
Leandro Joan Bustos <leandro@medhubtech.com>
2023-02-07 10:35:35 -0300

    feature/Change import user from entities to interfaces

--- src/mappers/user.ts
+++ src/mappers/user.ts
@@ -1,9 +1,8 @@
 import { UserAccesToken } from './../interfaces/user';
-import { User } from './../entities/user';
+import { User } from './../interfaces/user';
 
 export const mapUserAccessToken = (user: User):UserAccesToken => ({
     id: user.id,
     first_name: user.first_name,
     last_name: user.last_name,
-    type: user.type
 })
