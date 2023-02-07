1b2cb99ab43aeef7d3cfd706650a8c6004264e60
Leandro Joan Bustos <leandro@medhubtech.com>
2023-02-07 10:39:50 -0300

    feature/Add util for accesstoken decoding

--- src/utils/accessToken.ts
+++ src/utils/accessToken.ts
@@ -1,9 +1,13 @@
 import { config } from './../core/config';
 import { UserAccesToken } from './../interfaces/user';
-import { sign } from 'jsonwebtoken'
+import { JwtPayload, sign, verify } from 'jsonwebtoken'
 
-
-//TODO DEFINIR UNA SECRETPASSWORD QUE NO ESTE HARDCODED PARA JWT QUE 
 export const createAccessToken = async (user: UserAccesToken):Promise<string> => {
     return sign(user, config.jwt.secret, {algorithm: 'HS256'});
+}
+
+export const decodeAccessToken = async (accessToken: string):Promise<UserAccesToken> => {
+    const decoded = await verify(accessToken, config.jwt.secret) as JwtPayload
+    delete decoded.iat
+    return decoded as UserAccesToken
 }
