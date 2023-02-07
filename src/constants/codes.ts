6bc429762378b6b47503bdabffa0605a76e8a30c
Leandro Joan Bustos <leandro@medhubtech.com>
2023-02-07 10:27:17 -0300

    feature/Add forbidden status code 403

--- src/constants/codes.ts
+++ src/constants/codes.ts
@@ -9,6 +9,7 @@ export enum InternalCodes {
 export enum StatusCodes {
     OK = 200,
     UNAUTHORIZED = 401,
+    FORBIDDEN = 403,
     NOT_FOUND = 404,
     UNPROCESSABLE_ENTITY = 422,
     INTERNAL_SERVER_ERROR = 500,
