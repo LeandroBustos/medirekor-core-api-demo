a5ebb8040b701cd3e055b287708df6739296c06d
Leandro Joan Bustos <leandro@medhubtech.com>
2023-02-07 10:31:17 -0300

    feature/Create interfaces for custom request objects

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -1,12 +1,8 @@
 import { UserState, UsertTypes } from './../constants/user';
 
-export interface UserLogin {
-    user: string,
-    password: string
-};
-
 export interface User {
-    id: number;
+    id: string;
+    organization_id: string;
     first_name: string;
     last_name: string;
     email: string;
@@ -18,9 +14,29 @@ export interface User {
     type: UsertTypes;
 }
 
+export interface UserLogin {
+    user: User['email'],
+    password: User['password']
+};
+
 export interface UserAccesToken {
-    id: number;
-    first_name: string;
-    last_name: string;
-    type: UsertTypes
+    id: User['id'];
+    first_name: User['first_name'];
+    last_name: User['last_name'];
 }
+
+export interface UserCreation {
+    organization_id: User['organization_id'];
+    first_name: User['first_name'];
+    last_name: User['last_name'];
+    email: User['email'];
+    type: UsertTypes;
+}
+
+export interface UserCreationSerialize extends UserCreation {
+    id: User['id'];
+    password: User['password']
+    state: UserState;
+}
+
+export type UserUpdate = Pick<User, 'first_name' | 'last_name' | 'email' | 'state'>
